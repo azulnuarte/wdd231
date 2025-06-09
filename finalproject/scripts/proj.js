09880733fb9d1324a5fc519ccf7c40dc4d8ef442
azulnuarte <azulnuarte@gmail.com>
2025-06-09 12:35:25 -0300

    commit

---
+++ finalproject/scripts/proj.js
@@ -0,0 +1,21 @@
+const yearSpan = document.getElementById("copyright-year");
+if (yearSpan) {
+  const currentYear = new Date().getFullYear();
+  yearSpan.textContent = currentYear;
+}
+
+const lastModifiedParagraph = document.getElementById("last-modified");
+if (lastModifiedParagraph) {
+  lastModifiedParagraph.textContent = `Last Modified: ${document.lastModified}`;
+}
+
+const hamburgerElement = document.querySelector('#myButton');
+const navElement = document.querySelector('#animateme');
+if (hamburgerElement && navElement) {
+  hamburgerElement.addEventListener('click', () => {
+    navElement.classList.toggle('open');
+    hamburgerElement.classList.toggle('open');
+  });
+}
+
+
