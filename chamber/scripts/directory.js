6bc5fe085783c1c048adaa80070091568f4819d7
azulnuarte <azulnuarte@gmail.com>
2026-01-15 17:03:48 -0300

    commit

---
+++ chamber/scripts/directory.js
@@ -0,0 +1,41 @@
+const membersContainer = document.querySelector("#members");
+const gridButton = document.querySelector("#grid");
+const listButton = document.querySelector("#list");
+
+async function getMembers() {
+  const response = await fetch("data/members.json");
+  const members = await response.json();
+  displayMembers(members);
+}
+
+function displayMembers(members) {
+  membersContainer.innerHTML = "";
+
+  members.forEach(member => {
+    const card = document.createElement("section");
+    card.classList.add("member-card");
+
+    card.innerHTML = `
+      <img src="images/${member.image}" alt="${member.name} logo">
+      <h2>${member.name}</h2>
+      <p>${member.address}</p>
+      <p>${member.phone}</p>
+      <a href="${member.website}" target="_blank">Visit Website</a>
+      <p class="membership">${member.membership} Member</p>
+    `;
+
+    membersContainer.appendChild(card);
+  });
+}
+
+gridButton.addEventListener("click", () => {
+  membersContainer.classList.add("grid");
+  membersContainer.classList.remove("list");
+});
+
+listButton.addEventListener("click", () => {
+  membersContainer.classList.add("list");
+  membersContainer.classList.remove("grid");
+});
+
+getMembers();
